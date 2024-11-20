6ec26cbe560d74418eed115f23ded2688774b424
Rahul <rahul.mymail1@gmail.com>
2024-11-21 02:18:37 +0530

    Add: 1-1 msg

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import React, { useCallback, useEffect } from 'react'
-import {io} from 'socket.io-client'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
+import {io, Socket} from 'socket.io-client'
 
 interface SocketProviderProps {
     children? : React.ReactNode
@@ -13,23 +13,37 @@ interface ISocketContext {
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
 
+export const useSocket = () => {
+    const state = useContext(SocketContext);
+    if(!state) throw new Error('state is not define hee');
+    
+    return state
+}
+
 export const SocketProvider : React.FC<SocketProviderProps> = ({children}) => {
+    const [socket, setSocket] = useState<Socket>();
 
     const sendMessage: ISocketContext['sendMessage'] = useCallback((msg: string) => {
         console.log('send Message', msg);
+        if(socket){
+
+            socket.emit('event:message', {message: msg});
+        }
         
-    }, []);
+    }, [socket]);
 
     useEffect(()=> {
         const _socket = io('http://localhost:8081')
+        setSocket(_socket);
 
         return () => {
             _socket.disconnect();
+            setSocket(undefined);
         }
     }, [])
 
     return (
-        <SocketContext.Provider value={null}>
+        <SocketContext.Provider value={{sendMessage}}>
             {children}
         </SocketContext.Provider>
     )
