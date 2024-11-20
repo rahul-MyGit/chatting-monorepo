ba1d6babdbdc7e33725197e85e336b2c239e76b4
Rahul <rahul.mymail1@gmail.com>
2024-11-21 00:46:24 +0530

    base temp

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello from server')
+import http from 'http'
+import SocketService from './services/socket';
+
+async function init() {
+
+    const socketService = new SocketService();
+
+    const httpServer = http.createServer();
+    const PORT = process.env.PORT ? process.env.PORT :8080
+
+    socketService.io.attach(httpServer)
+
+    httpServer.listen(PORT, () => {
+        console.log(`Server running on ${PORT}`);
+    });
+}
+
+init();
---
+++ apps/backend/src/services/socket.ts
@@ -0,0 +1,13 @@
+import { Server } from 'socket.io'
+
+class SocketService {
+    private _io: Server
+    constructor() {
+        this._io = new Server()
+    }
+    get io() {
+        return this._io;
+    }
+}
+
+export default SocketService
